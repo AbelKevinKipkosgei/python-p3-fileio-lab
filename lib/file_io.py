ca1bbdf9d3b5f8def2f40f92e3509cbaca681c54
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-09-05 21:52:41 +0300

    Worked on file input output in Python.

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as test_file:
+        test_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as test_file:
+        test_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r", encoding="utf-8") as test_file:
+        for line in test_file:
+            return line
+
+#Function Calls
+write_file("test_file", "This is a test content.")
+append_file("test_file", "\nAppended content.")
+read_file("test_file")
